836d318b122a2426078acb75d253eba6530f25cb
James Secor <james.secor@gmail.com>
2023-02-20 15:36:22 -0500

    use circle when no images

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -7,6 +7,7 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { artists } from '../data/artists';
 import './Artists.css';
+import Box from '@mui/material/Box';
 
 export const NavItemArtists = 'Artists';
 
@@ -16,34 +17,31 @@ const Artists = () => {
     return (
         <Container>
             <Grid justifyContent={'center'} container={true} spacing={2}>
-                {artists.filter(artist => !artist.isNotMember).map((artist) => {
-                    return (
-                        <Grid
-                            item={true}
-                            justifyContent={'center'}
-                            xs={4}
-                            md={2.25}
-                            lg={1.6}
-                            sx={{ display: 'flex', flexDirection: 'column' }}
+                {artists.filter(artist => !artist.isNotMember).map((artist) => (
+                    <Grid
+                        item={true}
+                        justifyContent={'center'}
+                        xs={4}
+                        md={2.25}
+                        lg={1.6}
+                        sx={{ display: 'flex', flexDirection: 'column' }}
+                    >
+                        <ButtonBase onClick={() => navigateTo(`/artists/${artist.paramName}`)}>
+                            {artist.images.length
+                                ? <img className={'artists-image'} alt={'no image available'} src={`${process.env.REACT_APP_URL}/images/artists/${artist.images[0].src}`} />
+                                : <Box className={`artists-image`} sx={{ backgroundColor: 'lightblue' }} />
+                            }
+                        </ButtonBase>
+                        <Button
+                            variant={'text'}
+                            onClick={() => navigateTo(`/artists/${artist.paramName}`)}
                         >
-                            {artist.images.length 
-                                ? (
-                                <ButtonBase onClick={() => navigateTo(`/artists/${artist.paramName}`)}>
-                                    <img className={'artists-image'} alt={'no image available'} src={`${process.env.REACT_APP_URL}/images/artists/${artist.images[0].src}`} />
-                                </ButtonBase>
-                                )
-                                : null}
-                            <Button
-                                variant={'text'}
-                                onClick={() => navigateTo(`/artists/${artist.paramName}`)}
-                            >
-                                <Typography sx={{ color: 'black' }}>
-                                    {artist.name}
-                                </Typography>
-                            </Button>
-                        </Grid>
-                    );
-                })}
+                            <Typography sx={{ color: 'black' }}>
+                                {artist.name}
+                            </Typography>
+                        </Button>
+                    </Grid>
+                ))}
             </Grid>
         </Container>
     )
