6301cf7a47f3fec21e7c3c0921da4fe9a2bc56f1
James Secor <james.secor@gmail.com>
2024-09-29 20:18:34 -0400

    feat: basic events page

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Container, Stack, Typography } from '@mui/material';
+import { MdConstruction } from "react-icons/md";
 
 export const NavItemEvents = 'Events';
 
 const Events = () => {
     return (
-        <div>{'events'}</div>
+        <Container maxWidth={'sm'}>
+            <Stack alignItems={'center'}>
+                <MdConstruction size={133} />
+                <Typography
+                    variant={'body1'}
+                    sx={{
+                        paddingBottom: 2
+                    }}
+                >
+                    {'This page is under construction.'}
+                </Typography>
+                <Typography variant={'body1'}>
+                    {'For now, you should know that there are openings every first Friday of the month. Often during solo shows, the artist will hold some sort of event resembling an artist\'s talk, and we sometimes have other events such as poetry readings or panel discussions.'}
+                </Typography>
+            </Stack>
+        </Container>
     )
 };
 
