f8c163484917cb9cdedc2e3ff25ee811ceb31328
James Secor <james.secor@gmail.com>
2024-10-08 20:26:12 -0400

    feat: images

--- src/data/shows.ts
+++ src/data/shows.ts
@@ -19,9 +19,19 @@ export const shows: Array<IShow> = [
             'If you can\'t be there on opening day, Show 63 will continue through Sunday, October 27th, every Friday from 4 to 7 and Saturday-Sunday 11 to 5, or by appointment. <a href="/shows/show-61?open-contact-form=true">Contact Us</a>'
         ],
         images: [
-            { src: '2024-10/01.jpeg' },
-            { src: '2024-10/02.jpeg' },
-            { src: '2024-10/03.jpeg' }
+            { src: '2024-10/01.jpg' },
+            { src: '2024-10/02.jpg' },
+            { src: '2024-10/03.jpg' },
+            { src: '2024-10/04.jpg' },
+            { src: '2024-10/05.jpg' },
+            { src: '2024-10/06.jpg' },
+            { src: '2024-10/07.jpg' },
+            { src: '2024-10/08.jpg' },
+            { src: '2024-10/09.jpg' },
+            { src: '2024-10/10.jpg' },
+            { src: '2024-10/11.jpeg' },
+            { src: '2024-10/12.jpeg' },
+            { src: '2024-10/13.jpeg' }
         ]
     },
     {
@@ -41,6 +51,12 @@ export const shows: Array<IShow> = [
             'Rose does not shy away from the extremes. The same intensity and intimacy are found in a portrait of a grand-daughter, achingly sweet, and a heart-stopping tableau of George Floyd’s killing. Both are rendered with the same artistic vocabulary of large fields of color and simple figurative shapes. The paintings themselves—and one mobile —are too large and strong to walk by casually.',
             'It takes courage to mount a show like this. “I\'m feeling vulnerable,” Rose says. But her openness to both love and fear may allow the viewer as well to open up to the beauty and pain of our times.',
             'Rose is married to the painter Jonathan Rose. “We both paint,” she says, “but we try not to comment on each other’s work unless asked.” (Both smile.) They have kept at it for nearly 60 years. For 20 years they ran a B and B in the West Indies, leading art retreats and classes. Today they live in Thetford, Vermont and winter in Merida, Mexico, on the Yucatan peninsula.'
+        ],
+        images: [
+            { src: '2024-09/01.jpeg' },
+            { src: '2024-09/02.jpeg' },
+            { src: '2024-09/03.jpeg' },
+            { src: '2024-09/04.jpeg' }
         ]
     },
     {
